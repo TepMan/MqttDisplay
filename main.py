79202cb113a0d1af1036f2731c8de71e9bac327c
TepMan <tepper.andreas@gmail.com>
2021-11-06 19:33:27 +0100

    Kleinere Korrekturen, Anzeigedauer generell 5 Sek.

--- main.py
+++ main.py
@@ -10,57 +10,58 @@ from queue import Queue
 mq = Queue()
 
 # logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.INFO)
+
+# Create MQTT-Client
+mqtt_client = mqtt.Client("StatusPI")
 
 
-def on_message(client, userdata, message):
-    payload = str(message.payload.decode("utf-8"))
+def on_message(client, userdata, raw_msg):
+    payload = str(raw_msg.payload.decode("utf-8"))
     mq.put(payload)
-    logging.info("Message = " + payload)
+    logging.info("Message: " + payload)
 
 
 def on_log(client, userdata, level, buf):
-    logging.info("Log: " + buf)
+    logging.debug("Log: " + buf)
 
 
 try:
     display_init()
-    logging.info("Display init completed")
+    logging.debug("Display init completed")
 
-    client = mqtt.Client("StatusPI")
-    client.on_message = on_message
-    client.on_log = on_log
-    logging.info("MQTT Client started")
+    mqtt_client.on_message = on_message
+    mqtt_client.on_log = on_log
+    logging.debug("MQTT Client started")
 
-    client.connect(host="raspi4", port=1883, keepalive=60)
-    logging.info("Client connected")
+    mqtt_client.connect(host="raspi4", port=1883, keepalive=60)
+    logging.debug("Client connected")
 
-    client.subscribe("iob/#")
-    logging.info("Client subscribed")
+    mqtt_client.subscribe("iob/#")
+    logging.debug("Client subscribed")
 
-    client.loop_start()
-    logging.info("Client loop startes")
+    mqtt_client.loop_start()
+    logging.debug("Client loop startes")
 
     while True:
         while not mq.empty():
             message = mq.get()
             if message is None:
                 continue
+            logging.info("Output: " + message)
             display_message(message, 36)
             time.sleep(5)
             display_message(time.strftime('%H:%M') + ' Uhr', 48)
-            time.sleep(10)
-            # display_message('AAAAAAA')
-            # display_message('   AAAAA   ')
+            time.sleep(5)
 
 
 except IOError as e:
     logging.error(e)
 
 except KeyboardInterrupt:
-    logging.info("ctrl + c:")
+    logging.debug("ctrl + c:")
     display_cleanup()
-    logging.info("Display cleanup completed")
-    logging.info("Program end")
-    client.loop_stop()
+    logging.debug("Display cleanup completed")
+    logging.debug("Program end")
+    mqtt_client.loop_stop()
     exit()
